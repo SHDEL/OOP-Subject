1416982721987416dd32b5693221e3985cc46de7
Kongpob Worawutkasem <del2547.pv@gmail.com>
2023-11-28 13:11:20 +0700

    Update files

---
+++ Lec1.java
@@ -0,0 +1,9 @@
+public class Lec1 {
+    public static void main(String[] args) {
+        demo1();
+    }
+    static void demo1(){
+        Person p1 = new Person();
+        p1.set_attributes("del", 100);
+    }
+}
---
+++ Person.java
@@ -0,0 +1,14 @@
+/**
+ * Person
+ */
+public class Person {
+
+    String name;
+    int saving;
+
+    void set_attributes(String x , int y){
+        name = x;
+        saving = y;
+    }
+
+}
